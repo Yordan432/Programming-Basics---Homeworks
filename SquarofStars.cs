c74a54e8270eda8d1351e936a6f36e71d09435cb
Yordan432 <dani.583@abv.bg>
2017-01-21 14:38:31 +0200

    MyHomeWorkProgrammingBasics

---
+++ SquarofStars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SquareofStars
+{
+    class SquarofStars
+    {
+        static void Main()
+        { 
+            //НЕ РЕШЕНА ДОКРАЙ ЗАДАЧА
+
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('*' , n));
+
+            for (int i = 1; i <n-2; i++)
+            {
+                Console.Write(new string(' ' , n-2));
+                Console.Write("*");
+                Console.Write(new string('*', n));
+            }
+            Console.WriteLine();
+        }
+    }
+}
